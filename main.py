caa225559583df99846f4d44613d8047fa849ead
RashidZada <rashidzada6@gmail.com>
2022-12-04 14:23:39 +0500

    Adding some other component

--- main.py
+++ main.py
@@ -23,7 +23,27 @@ def main(page:Page):
         title=Text("My AppBar"),
         center_title=False,
         actions=[
+          IconButton(icons.FACEBOOK),
+          IconButton(icons.MESSENGER),
+          IconButton(icons.LOGIN),
+          IconButton(icons.PERSON_ADD),
+          PopupMenuButton(
 
+              items=[
+                  PopupMenuItem(text="item 1"),
+                  PopupMenuItem(text="item 2"),
+                  PopupMenuItem(text="item 3"),
+                  PopupMenuItem(text="item 4")
+
+              ]
+          )
+
+        ]
+    )
+    page.navigation_bar = NavigationBar(
+        destinations=[
+            NavigationDestination(label="Setting", icon=icons.SETTINGS),
+            NavigationDestination(label="FILE", icon=icons.FILE_OPEN)
         ]
     )
     page.add(App())
