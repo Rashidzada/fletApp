15b08b24ee88e92cc16eabb11927a21eeae2c412
RashidZada <rashidzada6@gmail.com>
2022-12-04 14:12:29 +0500

    AppBar added

--- main.py
+++ main.py
@@ -2,18 +2,33 @@ import flet
 
 from flet import *
 
+
 class App(UserControl):
     def build(self):
         return Container(
             content=Row(
                 controls=[
-                    Text("this is a first time to show ")
+                  Text('this is a first time to the main while')
                 ]
             )
         )
 
+
 def main(page:Page):
+    page.appbar = AppBar(
+        leading=Icon(icons.PALETTE_SHARP),
+        leading_width=40,
+        bgcolor=colors.BLACK,
+        color=colors.WHITE,
+        title=Text("My AppBar"),
+        center_title=False,
+        actions=[
+
+        ]
+    )
     page.add(App())
+
+
 
 if __name__ == '__main__':
     flet.app(target=main)
