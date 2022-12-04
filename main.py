67d6d1f7deab2a7ca685153fca59113c92a9bdea
RashidZada <rashidzada6@gmail.com>
2022-12-04 14:57:35 +0500

    Added listview

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class App(UserControl):
                   Tabs(
                       tabs=[
                           Tab(
-                              text="Add",
+                              text="Add"
                           )
                       ]
                   ),
